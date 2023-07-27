fd126ced1329a3d29af7b20278b26bbfbbbc2454
al-shp <alshp@tutanota.com>
2023-07-27 18:05:03 +0200

    added apps

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -38,12 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'tags.apps.TagsConfig',
     'recipes.apps.RecipesConfig',
-    'shopping_list.apps.ShoppingListConfig',
-    'favourites.apps.FavouritesConfig',
-    'subscriptions.apps.SubscriptionsConfig',
-    'ingredients.apps.IngredientsConfig',
     'api.apps.ApiConfig',
 ]
 
