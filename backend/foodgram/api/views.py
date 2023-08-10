6f8f909ef6e6f53c7ccecabf1fffd52ee82b9f45
al-shp <alshp@tutanota.com>
2023-08-10 17:20:59 +0200

    serializers

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 
-from recipes.models import Tag, Ingredient
 from api.serializers import TagSerialiser, IngredientSerializer
+from api.permissions import IsAdminAuthorOrReadOnly
+from recipes.models import Tag, Ingredient, Recipe
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
@@ -20,8 +21,11 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = None
 
 
-class RecipeViewSet():
-    pass
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    permission_classes = (IsAdminAuthorOrReadOnly, )
+    filter_backends = (DjangoFilterBackend,)
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
 
 class UserViewSet():
