3ff6716c88c146b0f2b6ed87089cf12d1e79c536
al-shp <alshp@tutanota.com>
2023-08-11 14:23:50 +0200

    filter

---
+++ backend/foodgram/api/filters.py
@@ -0,0 +1,37 @@
+from django_filters.rest_framework import FilterSet, filters
+
+from recipes.models import Recipe, Tag
+from users.models import User
+
+
+class RecipeFilter(FilterSet):
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name='tags__slug',
+        to_field_name='slug',
+    )
+    is_favorited = filters.BooleanFilter(
+        method='get_is_favorited_filter'
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_is_in_shopping_cart_filter'
+    )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'author', 'tags', 'is_favorited', 'is_in_shopping_cart',
+        )
+
+    def get_is_favorite_filter(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(favorites__user=user)
+        return queryset
+    
+    def get_is_in_shopping_cart_filter(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(carts__user=user)
+        return queryset
