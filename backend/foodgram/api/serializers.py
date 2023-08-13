820852e5222c9b502fe5552b7d8f23fdaf36deaf
al-shp <alshp@tutanota.com>
2023-08-13 12:15:26 +0200

    serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -3,6 +3,35 @@ from rest_framework import serializers
 from recipes.models import (
     Tag, Ingredient,
 )
+from users.models import User, Subscription
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'id', 'email', 'username', 'first_name',
+            'last_name', 'password',
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'id', 'email', 'username', 'first_name',
+            'last_name', 'is_subscribed'
+        )
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        return (request.user.is_authenticated
+                and Subscription.objects.filter(
+                    user=request.user, author=obj
+                ).exists())
 
 
 class TagSerialiser(serializers.ModelSerializer):
@@ -17,11 +46,3 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = '__all__'
-
-
-class UserSerializer(serializers.ModelSerializer):
-    pass
-
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    pass
