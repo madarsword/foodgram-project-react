46f080d13ebe93340e8f26db5209759f2263f729
al-shp <alshp@tutanota.com>
2023-08-16 18:50:53 +0200

    urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -5,22 +5,18 @@ from .views import (
     TagViewSet,
     IngredientViewSet,
     RecipeViewSet,
-    UserSubscriptionGetViewSet,
-    UserSubscriptionView,
+    UserViewSet,
 )
 
 
 router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
 router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 
 
 urlpatterns = [
-    path('users/subscriptions/',
-         UserSubscriptionGetViewSet.as_view({'get': 'list'})),
-    path('users/<int:user_id>/subscribe/',
-         UserSubscriptionView.as_view()),
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
